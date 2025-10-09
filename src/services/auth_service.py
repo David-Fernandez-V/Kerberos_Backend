fe70b6aa9fa1908c376127859f72b0c7bfbcc4f4
David-Fernandez-V <david.fernandez.v04@gmail.com>
2025-10-09 14:18:59 -0600

    Ahora los tokens de sesión duran 1 hora

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -21,7 +21,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(hours=1))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
